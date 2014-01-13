e6df40f6daf798b2fa0fb19e1d7ed0a5c08f20cb
potal <potal@localhost.localdomain>
2014-01-13 15:39:34 +0800

    linux workspace server directory

---
+++ gate_server/test_libevent.cpp
@@ -0,0 +1,59 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test_libevent.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年01月07日 14时38分25秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <event.h>
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+void set_non_block()
+{
+	int flags = fcntl(listen_fd,F_G
+	flags |= O_NONBLOCK;
+	fcntl(listen_fd,F_SETFL,flags);
+}
+
+int main()
+{
+	struct event ev_accept;
+	event_init();
+	struct sockaddr_in listen_addr;
+	int listen_port = 5555;
+	int listen_fd = socket(AF_INET,SOCK_STREAM,0);
+	memset(&listen_addr,0,sizeof(listen_addr));
+	listen_addr.sin_family = AF_INET;
+	listen_addr.sin_addr.s_addr = INADDR_ANY;
+	listen_addr.sin_port = htons(port);
+	
+	int resue_addr_on = 1;
+	setsockopt(listen_fd,SOL_SOCKET,SO_REUSEADDR,&resue_addr_on,sizeof(resue_addr_on));
+	
+	bind(listen_fd,(struct sockaddr*)&listen_addr,sizeof(listen_addr));
+	listen(listen_fd,1024);
+
+	set_non_block();
+
+	event_set(&ev_accept,listen_fd,EV_READ|EV_PERSIST,on_accecpt,NULL);
+	event_add(&ev_accept,NULL);
+
+	event_dispatch();
+
+	return 0;
+}
