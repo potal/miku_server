0ddb30d8914b7ff99b598c8bcbe42bdeada920b1
potal <bushimajia@126.com>
2014-01-29 03:01:13 +0800

    add circle buffer

--- socket_event_template/common/circle_buff.h
+++ socket_event_template/common/circle_buff.h
@@ -2,7 +2,10 @@
 #define CIRCLE_BUFF_H_
 
 #include <pthread.h>
+#include "auto_lock.h"
+
 const int default_buff_len = 0x4000;
+
 class CircleBuff
 {
 private:
@@ -63,6 +66,7 @@ CircleBuff::~CircleBuff()
 
 bool CircleBuff::AddBuff(char *buff,int buff_len)
 {
+	AutoLock lock(&buff_lock_);
 	if(buff_len > buff_len_)
 		return false;
 	if(remain_buff_ < buff_len)
@@ -103,19 +107,68 @@ bool CircleBuff::AddBuff(char *buff,int buff_len)
 
 bool CircleBuff::GetBuff(char *buff_out,int len_in,int &len_out)
 {
+	AutoLock lock(&buff_lock_);
+
+	if(NULL == buff_out)
+		return false;
 	if(buff_len_-remain_len_ > len_in)
 	{
 		len_out = len_in;
+		if(buff_tail_ >= buff_header_)//
+		{
+			memcpy(buff_out,buff_header_,len_in);
+			buff_header_ += len_in;
+			remain_len_ += len_in;
+			return true;
+		}
+		else
+		{
+			int tmp_header_end_len = static_cast<int>(buff_+buff_len_-buff_header_);
+			if(tmp_header_end_len >= len_in)
+			{
+				memcpy(buff_out,buff_header_,len_in);
+				buff_header_ += len_in;
+				remain_len_ += len_in;
+				return true;
+			}
+			else
+			{
+				memcpy(buff_out,buff_header_,tmp_header_end_len);
+				memcpy(buff_out+tmp_header_end_len,buff_,len_in-tmp_header_end_len);
+				buff_header_ = buff_+len_in-tmp_header_end_len;
+				remain_len_ += len_in;
+				return true;
+			}
+		}
 	}
-	else
+	else//copy all data in the buffer to out buffer
 	{
 		len_out = buff_len_-remain_len_;
+		if(buff_tail_ >= buff_header_)
+		{
+			memcpy(buff_out,buff_header_,static_cast<int>(buff_tail_-buff_header_));
+			remain_len_ = buff_len_;
+			buff_tail_ = buff_header_ = buff_;
+			return true;
+		}
+		else
+		{
+			int tmp_header_end_len = static_cast<int>(buff_+buff_len-buff_header_);
+			int tmp_start_tail_len = static_cast<int>(buff_tail_-buff_);
+			memcpy(buff_out,buff_header_,tmp_header_end_len);
+			memcpy(buff_out,buff_,tmp_start_tail_len);
+			remain_len_ = buff_len_;
+			buff_tail_ = buff_header_ = buff_;
+			return true;
+		}
 	}
 	return true;
 }
 
 bool CircleBuff::ResetBuff()
 {
+	AutoLock lock(&buff_lock_);
+
 	remain_len_ = buff_len_;
 	buff_header_ = buff_;
 	buff_tail_ = buff_;
@@ -124,6 +177,8 @@ bool CircleBuff::ResetBuff()
 
 void CircleBuff::ReleaseBuff()
 {
+	AutoLock lock(&buff_lock_);
+
 	if(buff_)
 	{
 		delete []buff_;
